10b982aa4e9401fac859e9d202baf324cf21862a
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-05 14:18:22 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,17 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  	# session[:form_params] = params.inspect
+  	@student = Student.create(params[:first_name], params[:last_name])
+  	redirect_to new_student_path
+  	#(student: @student)
+  end
+
+
+
+
 end
